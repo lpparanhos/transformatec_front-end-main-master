0e01de7c64811b02153d9b9a844036e2a0658278
lpparanhos <paranhos.pan@gmail.com>
2022-11-09 03:23:03 -0300

    0911220322 - criação do header homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import Head from "next/head";
+import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 import styles from "../styles/HomeNoAuth.module.scss";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Uni(dev)</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Uni(dev)" key="title" />
+        <meta
+          name="description"
+          content="O melhor conteúdo de programação você encontra aqui, de uma forma simples e fácil!"
+        />
       </Head>
 
-      <main></main>
+      <main>
+        <HeaderNoAuth/>
+      </main>
     </>
   );
 };
---
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { Button, Container } from "reactstrap";
+import styles from "./styles.module.scss";
+
+const HeaderNoAuth = function () {
+  return (
+    <>
+      <div className={styles.ctaSection}>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+        <p>Cadastre-se e tenha acesso aos cursos</p>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+      </div>
+      <Container className={styles.nav}>
+        <img
+          src="/logoUnidev.svg"
+          alt="Logo Uni(dev)"
+          className={styles.imgLogoNav}
+        />
+        <div>
+          <Link href="/login">
+            <Button className={styles.navBtn} outline>
+              Entrar
+            </Button>
+          </Link>
+          <Link href="/register">
+            <Button className={styles.navBtn} outline>
+              Quero fazer parte
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default HeaderNoAuth;
