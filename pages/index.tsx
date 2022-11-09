31fc458b641a6d1d12bb233e2e519fe5b1d3f1df
lpparanhos <paranhos.pan@gmail.com>
2022-11-09 01:42:39 -0300

    0911220142 - config do projeto, pastas e arquivos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,16 @@
 import Head from "next/head";
-import { GetStaticProps } from "next/types";
-import { ReactNode, useEffect } from "react";
-import Footer from "../src/components/common/footer";
-import CardsSection from "../src/components/homeNoAuth/cardsSection";
-import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
-import PresentationSection from "../src/components/homeNoAuth/presentationSection";
-import SlideSection from "../src/components/homeNoAuth/slideSection";
-import courseService, { CourseType } from "../src/services/courseService";
-import  styles  from "../styles/HomeNoAuth.module.scss";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import styles from "../styles/HomeNoAuth.module.scss";
 
-interface IndexPageProps{
-   chrildren?:  ReactNode;
-   course: CourseType[];               //3° ?
-
-}
-
-const HomeNoAuth = ({course}: IndexPageProps) =>{      //4° recebendo as props
-
-  useEffect(()=>{
-    AOS.init();
-  },[]);
-   
-
-
-  return(
+const HomeNoAuth = () => {
+  return (
     <>
-    <Head>
-    <title>TransformaTec</title>
-    <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>
-    <meta property="og:title" content="Tranformatec" key="title"/>
-    <meta name="description" content="Tranformatec" key="title"/>
-    </Head>
-    <main>
-      <div className={styles.sectionBackground} data-aos="fade-zoom-in" data-aos-duration="1600">
-      <HeaderNoAuth/>
-      <PresentationSection/>
-      </div>
+      <Head>
+        <title>Home</title>
+      </Head>
 
-      <div data-aos="fade-right" data-aos-duration="1200">
-      <CardsSection/>
-      </div>
-      
-      <div  data-aos="fade-up" data-aos-duration="1350">
-      <SlideSection newestCourses={course}/> 
-      </div>
-      
-      
-      <Footer/>                           
-    </main>
+      <main></main>
     </>
   );
-}; //1° Criar função a baixo ^
-
-export const getStaticProps: GetStaticProps = async ()=>{
-  const res = await courseService.getNewestCourses();               // 2° obter as informações por está função
-  return{
-    props:{
-      course: res.data,                                                // função para retorna capas e items do back end a cada 24 horas para sites mais staticos que não tanta alteração
-    },
-    revalidate: 3600 * 24,
-  };
 };
 
 export default HomeNoAuth;
