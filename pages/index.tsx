304047021cbe3b54897ee61fd0af8ad3e2dbdd21
lpparanhos <paranhos.pan@gmail.com>
2022-11-11 00:17:44 -0300

    1111220017 - criação de header generico

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "../src/components/common/footer";
 import CardsSection from "../src/components/homeNoAuth/cardSection";
 import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "../src/components/homeNoAuth/presentationSection";
 import SlideSection from "../src/components/homeNoAuth/slideSection";
 import courseService, { CourseType } from "../src/services/courseService";
-import styles from "../styles/HomeNoAuth.module.scss";
+import styles from "../styles/homeNoAuth.module.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 interface IndexPageProps {
   children?: ReactNode;
@@ -15,10 +17,15 @@ interface IndexPageProps {
 }
 
 const HomeNoAuth = function ({ course }: IndexPageProps) {
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  
   return (
     <>
       <Head>
-        <title>Uni(dev)</title>
+        <title>Unidev</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
         <meta property="og:title" content="Uni(dev)" key="title" />
         <meta
@@ -26,13 +33,21 @@ const HomeNoAuth = function ({ course }: IndexPageProps) {
           content="O melhor conteúdo de programação você encontra aqui, de uma forma simples e fácil!"
         />
       </Head>
-      <main>
-        <div className={styles.sectionBackground}>
+      <main >
+        <div
+          className={styles.sectionBackground}
+          data-aos="fade-zoom-in"
+          data-aos-duration="1600"
+        >
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <div data-aos="fade-right" data-aos-duration="1200">
         <CardsSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350" >
         <SlideSection newestCourses={course} />
+        </div>
         <Footer />
       </main>
     </>
